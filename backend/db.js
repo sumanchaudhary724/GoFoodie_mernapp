ac6f73e6a356decfae91aa557150f91f5ec2a83f
SumanChaudhary <sumanchaudhary724@gmail.com>
2023-12-13 22:38:45 +1100

    fetched data from db

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,16 @@
 const mongoose = require("mongoose");
 const mongoURI =
-  "mongodb+srv://gofoodie:gofoodie%4012@cluster0.y1njtgu.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://gofoodie:gofoodie%4012@cluster0.y1njtgu.mongodb.net/gofoodiemern?retryWrites=true&w=majority";
 
 const mongoDB = async () => {
   try {
     await mongoose.connect(mongoURI);
     console.log("Connected to MongoDB");
+
+    const collection = mongoose.connection.db.collection("food_items");
+    const data = await collection.find({}).toArray();
+
+    console.log("Fetched data:", data);
   } catch (error) {
     console.error("Error connecting to MongoDB:", error.message);
   }
