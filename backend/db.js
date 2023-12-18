2f195e4ba8513d7e0adc0e6c4b0095d5e71049d2
SumanChaudhary <sumanchaudhary724@gmail.com>
2023-12-19 00:26:45 +1100

    DisplayData backend done

--- backend/db.js
+++ backend/db.js
@@ -6,11 +6,12 @@ const mongoDB = async () => {
   try {
     await mongoose.connect(mongoURI);
     console.log("Connected to MongoDB");
+
     const fetched_data = await mongoose.connection.db.collection("food_items");
-    fetched_data.find({}).toArray(function (err, data) {
-      if (err) console.log(err);
-      else console.log(data);
-    });
+    const data = await fetched_data.find({}).toArray();
+    console.log("Retrieved data:", data);
+
+    global.food_items = data;
   } catch (error) {
     console.error("Error connecting to MongoDB:", error.message);
   }
--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,7 @@ app.get("/", (req, res) => {
 
 app.use(express.json());
 app.use("/api", require("./Routes/CreateUser.js"));
+app.use("/api", require("./Routes/DisplayData.js"));
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
