30cafb09bfdf03e2666b4e2bdc796dbbe9a80de6
SumanChaudhary <sumanchaudhary724@gmail.com>
2023-12-26 00:11:04 +1100

    created overlay styles----createPortal

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import Badge from "react-bootstrap/Badge";
+import Modal from "../Model";
+import Cart from "../screens/Cart";
+import { useCart } from "./ContextReducer";
 
 export default function Navbar() {
+  let data = useCart();
+  const [cartView, setCartView] = useState(false);
   const navigate = useNavigate();
   const handlelogout = () => {
     localStorage.removeItem("authToken");
@@ -50,7 +56,22 @@ export default function Navbar() {
             </div>
           ) : (
             <div>
-              <div className="btn bg-white text-success mx-2">My Cart</div>
+              <div
+                className="btn bg-white text-success mx-2"
+                onClick={() => {
+                  setCartView(true);
+                }}
+              >
+                My Cart
+                <Badge pill bg="danger">
+                  {data.length}
+                </Badge>
+              </div>
+              {cartView ? (
+                <Modal onClose={() => setCartView(false)}>
+                  <Cart />
+                </Modal>
+              ) : null}
               <div
                 className="btn bg-white text-danger mx-2"
                 onClick={handlelogout}
