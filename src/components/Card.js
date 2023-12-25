fc1bd9e8ddf78a1518ddea82f0b6a7a3c6911a01
SumanChaudhary <sumanchaudhary724@gmail.com>
2023-12-25 20:10:58 +1100

    Items add to cart and total price done

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useDispatchCart, useCart } from "./ContextReducer";
 
 export default function Card(props) {
-  let dispatch = useDispatchCart(); // Getting the dispatch function
+  const dispatch = useDispatchCart(); // Getting the dispatch function
   const data = useCart(); // Getting the cart state
+  const priceRef = useRef();
   const options = props.options || {};
   const priceOptions = Object.keys(options);
   const [qty, setQty] = useState(1);
@@ -13,7 +14,7 @@ export default function Card(props) {
       type: "ADD",
       id: props.foodItem._id,
       name: props.foodItem.name,
-      price: props.finalPrice,
+      price: finalPrice,
       qty: qty,
       size: size,
     });
@@ -21,6 +22,11 @@ export default function Card(props) {
   };
 
   let finalPrice = qty * parseInt(options[size]);
+
+  useEffect(() => {
+    setSize(priceRef.current.value);
+  }, []);
+
   return (
     <div className="mt-3">
       <div className="card mt-3" style={{ width: "18rem", maxHeight: "360px" }}>
@@ -47,6 +53,7 @@ export default function Card(props) {
             {priceOptions.length > 0 && (
               <select
                 className="m-2 h-100 bg-success rounded"
+                ref={priceRef}
                 onChange={(e) => setSize(e.target.value)}
               >
                 {priceOptions.map((data) => (
