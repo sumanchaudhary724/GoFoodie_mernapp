506a4fc299734ce6a43c5789e1e7f18537b708cc
SumanChaudhary <sumanchaudhary724@gmail.com>
2023-12-26 19:28:01 +1100

    Add to cart Update functionality done

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,23 +2,58 @@ import React, { useEffect, useRef, useState } from "react";
 import { useDispatchCart, useCart } from "./ContextReducer";
 
 export default function Card(props) {
-  const dispatch = useDispatchCart(); // Getting the dispatch function
-  const data = useCart(); // Getting the cart state
+  const dispatch = useDispatchCart();
+  const data = useCart();
   const priceRef = useRef();
   const options = props.options || {};
   const priceOptions = Object.keys(options);
+  let foodItem = props.foodItem; // Change this line to use props.foodItem
   const [qty, setQty] = useState(1);
   const [size, setSize] = useState("");
+
   const handleAddToCart = async () => {
+    let food = [];
+    for (const item of data) {
+      if (item.id === foodItem?._id) {
+        food = item;
+        break;
+      }
+    }
+    console.log(food);
+    console.log(new Date());
+    if (food.length !== 0) {
+      if (food.size === size) {
+        await dispatch({
+          type: "UPDATE",
+          id: foodItem._id,
+          price: finalPrice,
+          qty: qty,
+        });
+        return;
+      } else if (food.size !== size) {
+        await dispatch({
+          type: "ADD",
+          id: foodItem._id,
+          name: foodItem.name,
+          price: finalPrice,
+          qty: qty,
+          size: size,
+          img: props.ImgSrc,
+        });
+        console.log("Size different so simply ADD one more to the list");
+        return;
+      }
+      return;
+    }
+
     await dispatch({
       type: "ADD",
-      id: props.foodItem._id,
-      name: props.foodItem.name,
+      id: foodItem._id,
+      name: foodItem.name,
       price: finalPrice,
       qty: qty,
       size: size,
     });
-    console.log(data);
   };
 
   let finalPrice = qty * parseInt(options[size]);
